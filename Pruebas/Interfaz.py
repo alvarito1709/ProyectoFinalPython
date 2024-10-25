77a4690763008352f0a0fd50261060b139e68d72
EmilioMaidana <emaidana@frba.utn.edu.ar>
2024-10-25 02:06:31 -0300

    Añado modificaciones del codigo Interfaz, unico boton Login y validaciondes de usuario y contraseña dentro de la base de datos usuario, agrego ademas el script usuarios.sql para la creacion de la misma

--- Pruebas/Interfaz.py
+++ Pruebas/Interfaz.py
@@ -1,22 +1,81 @@
-#GRUPO 10: Cifuentes Juan, Ortiz Alvaro, Maidana Emilio
-#DIA Y HORARIO: 12/11 - 20:40 HS
-#Crear un programa para el manejo de viajes de una empresa de turismo teniendo en cuenta
-#los siguientes aspectos:
+# GRUPO 10: Cifuentes Juan, Ortiz Alvaro, Maidana Emilio
+# DIA Y HORARIO: 12/11 - 20:40 HS
+# Crear un programa para el manejo de viajes de una empresa de turismo teniendo en cuenta
+# los siguientes aspectos:
 
-#1) El sistema debe ser de acceso restringido sólo para algunas opciones especiales (a
-#considerar según el grupo de trabajo)
-#2) Planificación de viajes, destinos, duración y precios de los mismos
-#3) Salida de comprobantes por pantalla.
-
-
-
-
-
+# 1) El sistema debe ser de acceso restringido sólo para algunas opciones especiales (a
+# considerar según el grupo de trabajo)
+# 2) Planificación de viajes, destinos, duración y precios de los mismos
+# 3) Salida de comprobantes por pantalla.
 
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+import pyodbc
+#conecto a la base de datos
+
+
+def ventana_user(lienzo):
+    lienzo.destroy()
+    lienzo = tk.Tk()
+    lienzo.geometry("800x400")
+    lienzo.title("User")
+    #recuadro destino
+    groupbox = LabelFrame(lienzo, text="Seleccione el destino", padx=5, pady=5)
+    groupbox.grid(row=1, column=0, padx=0, pady=0)
+    labelDest = Label(groupbox, text="Destino ", width=13, font=("Arial", 12)).grid(row=1, column=0)
+    labeldestino = Label(groupbox, text="Seleccione el destino: ", width=20, font=("Arial", 12)).grid(row=1,column=0)
+    select_destino = tk.StringVar()
+    combo = ttk.Combobox(lienzo, values=["Copacabana", "United States", "Islas Galapagos", "Buenos Aires"],width=30, textvariable=select_destino).grid(row=1, column=1)
+    lienzo.mainloop()
+    return
+
+
+def validarusuario(lienzo, user, pw):
+    try:
+        conexion = pyodbc.connect(
+            r"Driver={SQL Server};SERVER=DESKTOP-MM59BEH\SQLEXPRESS;DATABASE=DB_USUARIOS;Trusted_Connection=yes;")
+        print("Conexión exitosa")
+
+        cursor = conexion.cursor()
+
+        # Consulta para verificar usuario, contraseña y rol
+        query = 'SELECT rol FROM usuarios WHERE usuario = ? AND contraseña = ?'
+        cursor.execute(query, (user.get(), pw.get()))
+
+        result = cursor.fetchone()
+
+        if result:
+            rol = result[0]
+            if rol == 1:  # Si rol es True (1)
+                print("Usuario Administrador")
+                ventana_adm(lienzo)  # Llamar a la ventana de administrador
+            else:  # Si rol es False (0)
+                print("Usuario Normal")
+                ventana_user(lienzo)  # Llamar a la ventana de usuario
+        else:
+            messagebox.showerror("Error", "Usuario o contraseña incorrectos")
+
+        cursor.close()
+        conexion.close()
+
+    except pyodbc.Error as error:
+        print("Error al conectar a la base de datos:", error)
+
+
+def ventana_adm(lienzo):
+    lienzo.destroy()
+    lienzo = tk.Tk()
+    lienzo.geometry("550x50")
+    lienzo.title("Administrador")
+    groupbox = LabelFrame(lienzo, text="Seleccione una opcion", padx=50, pady=5)
+    groupbox.grid(row=1, column=0, padx=0, pady=0)
+
+    Button(groupbox, text="Editar Destino", width=20).grid(row=1, column=0)
+    Button(groupbox, text="Agregar Destino", width=20).grid(row=1, column=1)
+    Button(groupbox, text="Actualizar Destino", width=20).grid(row=1, column=2)
+
 
 class LoginUsuario:
     def login():
@@ -25,35 +84,33 @@ class LoginUsuario:
             lienzo.geometry("800x400")
             lienzo.title("Login")
 
-            #recuadro de Login
-            groupbox = LabelFrame(lienzo, text="Login", padx=5, pady=5 )
+            # recuadro de Login
+            # recuadro de Login
+            groupbox = LabelFrame(lienzo, text="Login", padx=5, pady=5)
             groupbox.grid(column=0, row=0, padx=220, pady=20)
-            labelname= Label(groupbox, text="Username: ", width=13, font=("Arial", 12)).grid(row=0,column=0)
-            textboxusername= Entry(groupbox).grid(row = 0, column = 2)
 
-            groupbox.grid(column=0, row=0, columnspan=10)
-            labelpw = Label(groupbox, text="Password: ", width=13, font=("Arial", 12)).grid(row=1, column=0)
-            textboxpw = Entry(groupbox).grid(row=1, column=2)
-            
-            Button(groupbox,text="Soy admin", width= 10).grid(row=2, column = 0)
-            Button(groupbox, text="Soy Cliente", width=10).grid(row=2, column=2)
+            # Variables para almacenar el contenido de los Entry
+            username = StringVar()
+            password = StringVar()
 
-            #recuadro de paquetes
-            usuario=0
+            labelname = Label(groupbox, text="Username: ", width=13, font=("Arial", 12))
+            labelname.grid(row=0, column=0)
+            textboxusername = Entry(groupbox, textvariable=username)
+            textboxusername.grid(row=0, column=2)
 
-            groupbox = LabelFrame(lienzo, text="Seleccione el destino", padx=5, pady=5)
-            groupbox.grid(row=1, column=0, padx=0, pady=0)
-            labelDest = Label(groupbox, text="Destino ", width=13, font=("Arial", 12)).grid(row=1, column=0)
-            labeldestino = Label(groupbox, text="Seleccione el destino: ", width=20, font=("Arial", 12)).grid(row=1,column=0)
-            select_destino = tk.StringVar()
-            combo = ttk.Combobox(lienzo, values=["Copacabana", "United States", "Islas Galapagos", "Buenos Aires"],width=30, textvariable=select_destino).grid(row=1, column=1)
+            labelpw = Label(groupbox, text="Password: ", width=13, font=("Arial", 12))
+            labelpw.grid(row=1, column=0)
+            textboxpw = Entry(groupbox, textvariable=password, show='*')  # 'show' para ocultar la contraseña
+            textboxpw.grid(row=1, column=2)
 
+            Button(groupbox, text="Login", command=lambda: validarusuario(lienzo, username, password), width=10).grid(row=2, column=1)
 
-
+            # recuadro de paquetes
 
             lienzo.mainloop()
 
 
         except ValueError as error:
             print("Error al mostrar la interfaz, error: {}".format(error))
+
     login()
---
+++ Pruebas/ScriptUsuarios.sql
@@ -0,0 +1,35 @@
+-- Crear la base de datos DBCLIENTES si no existe
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'DB_USUARIOS')
+BEGIN
+    CREATE DATABASE DB_USUARIOS;
+END
+GO
+
+-- Usar la base de datos DBCLIENTES
+USE DB_USUARIOS;
+GO
+
+-- Crear la tabla clientes
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usuarios]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE usuarios (
+        id INT IDENTITY(1,1) PRIMARY KEY,
+        usuario NVARCHAR(50),
+        contraseña NVARCHAR(50),
+        rol BIT,
+        email NVARCHAR(100)
+    );
+END
+GO
+
+-- Insertar 20 registros de ejemplo
+INSERT INTO usuarios(usuario, contraseña, rol, email) VALUES
+('Pepito', '123', '1', 'juan@example.com'),
+('Fulano', '12345', '0', 'maria@example.com'),
+('Carlos', 'perro', '0', 'carlos@example.com'),
+('Emilio', '123Roger', '1', 'emaidana@frba.utn.edu.ar');
+GO
+
+-- Seleccionar todos los registros de la tabla clientes
+SELECT * FROM usuarios;
+GO
