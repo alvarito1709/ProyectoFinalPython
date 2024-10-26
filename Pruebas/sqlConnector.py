3f5af8534d8fb9dd206c8d58b8dd2faddfcecc58
Alvaro <alvaro9943310@gmail.com>
2024-10-26 13:27:47 -0300

    [FEATURE]: Alta de paquetes
    Función de alta de paquetes definida en archivo sqlConnector

--- Pruebas/sqlConnector.py
+++ Pruebas/sqlConnector.py
@@ -1,8 +1,9 @@
 import pyodbc
 from tkinter import *
+from ClassPaquete import Paquete
+from ClassUser import User
 
-
-
+#parametros para la conexion a la BBDD
 parametros_conexion = (
     r'DRIVER={SQL Server};'
     #Base de datos EMILIO SERVER=DESKTOP-MM59BEH\SQLEXPRESS
@@ -16,6 +17,8 @@ parametros_conexion = (
 
 def validarusuario(user, pw):
 	try:
+		
+		#utiliza los parametros declarados anteriormente para conectarse a la BBDD
 		conexion = pyodbc.connect(parametros_conexion)
 		print("Conexión exitosa")
 
@@ -52,31 +55,78 @@ def validarusuario(user, pw):
 		print("Error al conectar a la base de datos:", error)
 		
 
-def usuarioNuevo(usuario, contrasena, rol, email):
+def usuarioNuevo(user : User):
 	
-	try:
-		conexion = pyodbc.connect(parametros_conexion)
-		print("Conexión exitosa")
+	if isinstance(user, User):
+	
+		try:
+			conexion = pyodbc.connect(parametros_conexion)
+			print("Conexión exitosa")
 
-		cursor = conexion.cursor()
+			cursor = conexion.cursor()
 
-        # Consulta para verificar usuario, contraseña y rol
-		query = "INSERT INTO usuarios (usuario, contraseña, rol, email) VALUES(?,?,?,?)"
+			# Query para insertar nuevo usuario
+			query = "INSERT INTO usuarios (usuario, contraseña, rol, email) VALUES(?,?,?,?)"
+			
+			try:
+				
+				#ejecuta la query que declaramos anteriormente
+				cursor.execute(query, (user.usuario, user.contrasena, user.rol, user.email))
+
+				conexion.commit()
+				
+			except pyodbc.Error as err:
+				
+				print("error al agregar usuario", err)
+				
+				
+
+			cursor.close()
+			conexion.close()
+
+		except pyodbc.Error as error:
+			print("Error al conectar a la base de datos:", error.with_traceback)
+			
+	else:
+		print("El objeto tiene que ser de tipo User")
+		
+		
+
+
+
+def paqueteNuevo(paquete : Paquete):
+	
+	if isinstance(paquete, Paquete):
 		
 		try:
-			cursor.execute(query, (usuario, contrasena, rol, email))
+			conexion = pyodbc.connect(parametros_conexion)
+			print("Conexión exitosa")
 
-			conexion.commit()
+			cursor = conexion.cursor()
+
+			# Query para insertar nuevo usuario
+			query = "INSERT INTO paquetes (destino, duracion, precio, stock) VALUES(?,?,?,?)"
 			
-		except pyodbc.Error as err:
+			try:
+					
+				#ejecuta la query que declaramos anteriormente
+				cursor.execute(query, (paquete.destino, paquete.duracion, paquete.precio, paquete.stock))
+
+				conexion.commit()
+					
+			except pyodbc.Error as err:
+					
+				print("error al agregar paquete", err)
+				
+				
+
+			cursor.close()
+			conexion.close()
+
+		except pyodbc.Error as error:
+			print("Error al conectar a la base de datos:", error.with_traceback)
 			
-			print("error al agregar usuario", err)
-			
-            
+	else:
+		print("El objeto debe ser de tipo Paquete")
 
-		cursor.close()
-		conexion.close()
-
-	except pyodbc.Error as error:
-		print("Error al conectar a la base de datos:", error.with_traceback)
-
+		
